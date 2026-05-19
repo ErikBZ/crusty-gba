0324f5c60cea96c9f697cd281d8c5c0e3f2c29f4
Brandon Zapata <zapatabrandon@gmail.com>
2026-05-19 15:39:20 -0400

    Create lib to use in the test harness

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod cli;
+pub mod gba;
+pub mod ppu;
+pub mod renderer;
+pub mod utils;
+
+// TODO: Get rid of this since it messes with the use's in the submodules
+pub use crate::gba::system::SystemMemory;
+pub use gba::cpu::Cpu;
